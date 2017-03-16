4f610f6eb299f332c32bea0f8ef680cadb82153d
sualko <klaus@jsxc.org>
2017-03-16 15:09:34 +0100

    use dot notation

--- strophe.mam.js
+++ strophe.mam.js
@@ -3,7 +3,7 @@
  *
  * This file is MIT/X11 licensed. Please see the
  * LICENSE.txt file in the source package for more information.
- * 
+ *
  * Modified by: Chris Tunbridge (github.com/Destreyf/)
  * Updated to support v0.3 of the XMPP XEP-0313 standard
  * http://xmpp.org/extensions/xep-0313.html
@@ -25,10 +25,11 @@ Strophe.addConnectionPlugin('mam', {
             type:'set',
             id:jid
         };
+        options = options || {};
         var mamAttr = {xmlns: Strophe.NS.MAM};
-        if (!!options['queryid']) {
-            mamAttr.queryid = options['queryid'];
-            delete options['queryid'];
+        if (!!options.queryid) {
+            mamAttr.queryid = options.queryid;
+            delete options.queryid;
         }
         var iq = $iq(attr).c('query', mamAttr).c('x',{xmlns:'jabber:x:data', type:'submit'});
 
@@ -44,10 +45,10 @@ Strophe.addConnectionPlugin('mam', {
         }
         iq.up();
 
-        var onMessage = options['onMessage'];
-        delete options['onMessage'];
-        var onComplete = options['onComplete'];
-        delete options['onComplete'];
+        var onMessage = options.onMessage;
+        delete options.onMessage;
+        var onComplete = options.onComplete;
+        delete options.onComplete;
         iq.cnode(new Strophe.RSM(options).toXML());
 
         this._c.addHandler(onMessage, Strophe.NS.MAM, 'message', null);
